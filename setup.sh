f3b3937bf236e0bd024e64dc242c4924660eacc3
Ash <51839088+0xAsh@users.noreply.github.com>
2022-06-06 00:40:18 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+!/bin/bash
+
+echo "[*] Installing package dependencies..."
+apt-get install -y libssl-dev libffi-dev python-dev build-essential
+echo "[+] Packages installed"
+echo "[*] Installing Poetry..."
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+echo "[+] Poetry installed"
+echo "[*] Installing CME + Poetry..."
+git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec
+cd CrackMapExec
+poetry install
+poetry run crackmapexec
+echo "[+] CME + Poetry installed"
+cd ../
+if [ ! -d "./loot" ]; then
+        mkdir loot
+fi
