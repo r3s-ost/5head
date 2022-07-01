c917f2d2fb1ad952fbe12dd72607ffeb199bc592
Ash <asher.andree@gmail.com>
2022-07-01 14:12:15 -0400

    done with setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,11 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+echo "[*] Logging verbose install output in install.log..."
+
 ## Package + Poetry install
 echo "[*] Installing package dependencies..."
-apt-get install -y python3-venv
+apt-get install -y python3-venv >> install.log
 echo "[+] Packages installed"
 
 
@@ -26,27 +28,59 @@ echo "[+] Loot directory created"
 
 ## CME install
 echo "[*] Grabbing CME..."
-wget https://github.com/byt3bl33d3r/CrackMapExec/releases/download/v5.2.2/cme-ubuntu-latest.zip -O deps/cme-ubuntu-latest.zip
-unzip deps/cme-ubuntu-latest.zip -d deps
+wget https://github.com/byt3bl33d3r/CrackMapExec/releases/download/v5.2.2/cme-ubuntu-latest.zip -O deps/cme-ubuntu-latest.zip &> /dev/null
+unzip deps/cme-ubuntu-latest.zip -d deps >> install.log
 chmod +x deps/cme
+rm deps/cme-ubuntu-latest.zip
 echo "[+] CME downloaded"
 
 ## Responder setup
-git clone https://github.com/lgandx/Responder deps/Responder
+echo "[*] Grabbing lgandx fork of Responder..."
+git clone https://github.com/lgandx/Responder deps/Responder &> /dev/null
+echo "[+] Responder downloaded"
+echo "[*] Setting Responder.conf settings..."
 sed -i 's/ Random/ 1122334455667788/g' deps/Responder/Responder.conf
 sed -i 's/SMB = On/SMB = Off/g' deps/Responder/Responder.conf
 sed -i 's/HTTP = On/HTTP = Off/g' deps/Responder/Responder.conf
+echo "[+] Responder.conf configured"
 
 
 ## Impacket install
-wget https://github.com/SecureAuthCorp/impacket/releases/download/impacket_0_10_0/impacket-0.10.0.tar.gz -O deps/impacket-0.10.0.tar.gz
-tar xvf deps/impacket-0.10.0.tar.gz -C deps/
+echo "[*] Installing Impacket..."
+wget https://github.com/SecureAuthCorp/impacket/releases/download/impacket_0_10_0/impacket-0.10.0.tar.gz -O deps/impacket-0.10.0.tar.gz &> /dev/null
+tar xvf deps/impacket-0.10.0.tar.gz -C deps/ >> install.log
 rm deps/impacket-0.10.0.tar.gz
 cd deps/impacket-0.10.0
-virtualenv -p python3 .
+virtualenv -p python3 . >> ../../install.log
 source bin/activate
-python3 -m pip install .
-pip install dsinternals
+python3 -m pip install . >> ../../install.log
+pip install dsinternals >> ../../install.log
 deactivate
 cd ../../
+echo "[+] Impacket installed"
 
+
+## Mitm6 install
+echo "[*] Installing mitm6..."
+pip install mitm6 --log install.log &> /dev/null
+echo "[+] mitm6 installed"
+
+
+## LdapRelayScan install
+echo "[*] Installing LdapRelayScan..."
+git clone https://github.com/zyn3rgy/LdapRelayScan deps/LdapRelayScan &> /dev/null
+cd deps/LdapRelayScan
+virtualenv -p python3 . >> ../../install.log
+source bin/activate
+pip install -r requirements.txt >> ../../install.log
+deactivate
+cd ../../
+echo "[+] LdapRelayScan installed"
+
+## bloodhound-python install
+echo "[*] Installing bloodhound-python..."
+pip install bloodhound --log install.log &> /dev/null
+echo "[+] bloodhound-python installed"
+
+
+echo "[+] Installation complete!"
